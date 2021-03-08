b12371a71efe15cedce7e1526f1b9778ae97676b
resultunay@gmail.com <resultunay@gmail.com>
2021-03-08 21:45:23 +0300

    Add project files.

---
+++ ReferenceTypes/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int, decimal, float, enum, boolean = value types
+            //int sayi1 = 10;
+            //int sayi2 = 20;
+
+            //sayi1 = sayi2;
+
+            //sayi2 = 100;
+
+            //Console.WriteLine("Sayı 1: "+sayi1);
+
+            //arrays, class, interface... reference types
+            //int[] sayilar1 = new int[] { 1, 2, 3 };
+            //int[] sayilar2 = new int[] { 10, 20, 30 };
+
+            //sayilar1 = sayilar2;
+            //sayilar2[0] = 1000;
+
+            //Console.WriteLine("Sayılar1[0]= "+sayilar1[0]);
+
+            Person person1 = new Person();
+            Person person2 = new Person();
+            person1.FirstName = "Mustafa";
+
+            person2 = person1;
+            person1.FirstName = "Bilal";
+
+            //Console.WriteLine(person2.FirstName);
+
+
+            Customer customer = new Customer();
+            //customer.FirstName = "Tuğba";
+            customer.CreditCardNumber = "1234684646";
+            Employee employee = new Employee();
+            employee.FirstName = "Aysel";
+
+            Person person3 = customer;
+            customer.FirstName = "Burçak";
+
+            //Console.WriteLine(((Customer)person3).CreditCardNumber);
+
+            PersonManager personManager = new PersonManager();
+            personManager.Add(employee);
+
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    //base class :  Person
+    class Customer:Person
+    {
+        public string CreditCardNumber { get; set; }
+    }
+
+    class Employee:Person
+    {
+        public int EmployeeNumber { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(Person person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
